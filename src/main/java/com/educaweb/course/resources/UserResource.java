31ed1ee30c11006cc08875e1fd57b0884eb19183
douglas s dos anjos <douglasdsda@gmail.com>
2019-08-27 22:18:10 -0300

    Service layer, component registration

--- src/main/java/com/educaweb/course/resources/UserResource.java
+++ src/main/java/com/educaweb/course/resources/UserResource.java
@@ -1,20 +1,36 @@
 package com.educaweb.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educaweb.course.entities.User;
+import com.educaweb.course.services.UserService;
 
 @RestController
 @RequestMapping("/users" )
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "joao", "joao@teste", "42342-342", "123");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> users = service.findAll();
+		//return ResponseEntity.ok().body(u);
+		return ResponseEntity.ok().body(users);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<User> findId(@PathVariable Long id){
+		User user = service.findById(id);
+		//return ResponseEntity.ok().body(u);
+		return ResponseEntity.ok().body(user);
 	}
 
 }
---
+++ src/main/java/com/educaweb/course/services/UserService.java
@@ -0,0 +1,24 @@
+package com.educaweb.course.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educaweb.course.entities.User;
+import com.educaweb.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id){
+		return repository.findById(id).orElse(null);
+	}
+}
