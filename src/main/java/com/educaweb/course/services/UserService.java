1c4fa93e085b55b6160ff23fe9a47df27151eda4
douglas s dos anjos <douglasdsda@gmail.com>
2019-09-10 19:40:55 -0300

    User insert

--- src/main/java/com/educaweb/course/services/UserService.java
+++ src/main/java/com/educaweb/course/services/UserService.java
@@ -21,4 +21,8 @@ public class UserService {
 	public User findById(Long id){
 		return repository.findById(id).orElse(null);
 	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
 }
