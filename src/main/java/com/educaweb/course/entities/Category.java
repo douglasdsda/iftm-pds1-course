134537e985504b4512fe7c8501d1185d64b96d13
douglas s dos anjos <douglasdsda@gmail.com>
2019-09-10 21:47:20 -0300

    Category

--- src/main/java/com/educaweb/course/entities/Category.java
+++ src/main/java/com/educaweb/course/entities/Category.java
@@ -24,7 +24,7 @@ public class Category implements Serializable {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+	private Long id;
 
 	private String name;
 
@@ -35,7 +35,7 @@ public class Category implements Serializable {
 	public Category() {
 	}
 
-	public Category(Integer id, String name) {
+	public Category(Long id, String name) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -45,11 +45,11 @@ public class Category implements Serializable {
 		return products;
 	}
 
-	public Integer getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
--- src/main/java/com/educaweb/course/services/CategoryService.java
+++ src/main/java/com/educaweb/course/services/CategoryService.java
@@ -1,13 +1,15 @@
 package com.educaweb.course.services;
 
 import java.util.List;
-import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.educaweb.course.entities.Category;
 import com.educaweb.course.repositories.CategoryRepository;
+import com.educaweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -20,7 +22,31 @@ public class CategoryService {
 	}
 	
 	public Category findById(Long id){
-		Optional<Category> obj =  repository.findById(id);
-		return obj.get();
+		return  repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+	
+	
+	
+	public Category insert(Category obj) {
+		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+	
+	public Category update(Long id, Category obj) {
+		try {
+		Category entity = repository.getOne(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+		} catch(EntityNotFoundException e) {
+			e.printStackTrace();
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Category entity, Category obj) {
+		entity.setName(obj.getName());
 	}
 }
