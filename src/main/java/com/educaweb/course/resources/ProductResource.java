5f6b30b076fbc0e29475e90c07fb964f1b132d79
douglas s dos anjos <douglasdsda@gmail.com>
2019-09-24 21:44:13 -0300

    Product Insert

--- src/main/java/com/educaweb/course/resources/ProductResource.java
+++ src/main/java/com/educaweb/course/resources/ProductResource.java
@@ -1,15 +1,22 @@
 package com.educaweb.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.educaweb.course.dto.ProductCategoriesDTO;
 import com.educaweb.course.dto.ProductDTO;
+import com.educaweb.course.dto.UserDTO;
+import com.educaweb.course.dto.UserInsertDTO;
 import com.educaweb.course.services.ProductService;
 
 @RestController
@@ -30,6 +37,15 @@ public class ProductResource {
 	public ResponseEntity<ProductDTO> findId(@PathVariable Long id){
 		ProductDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping()
+	public ResponseEntity<ProductDTO> insert(@RequestBody ProductCategoriesDTO dto) {
+		ProductDTO newDto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newDto.getId()).toUri();
+
+		return ResponseEntity.created(uri).body(newDto);
+
 	}
 	
 
--- src/main/java/com/educaweb/course/services/ProductService.java
+++ src/main/java/com/educaweb/course/services/ProductService.java
@@ -5,9 +5,14 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.educaweb.course.dto.CategoryDTO;
+import com.educaweb.course.dto.ProductCategoriesDTO;
 import com.educaweb.course.dto.ProductDTO;
+import com.educaweb.course.entities.Category;
 import com.educaweb.course.entities.Product;
+import com.educaweb.course.repositories.CategoryRepository;
 import com.educaweb.course.repositories.ProductRepository;
 import com.educaweb.course.services.exceptions.ResourceNotFoundException;
 
@@ -17,6 +22,9 @@ public class ProductService {
 	@Autowired
 	private ProductRepository repository;
 	
+	@Autowired
+	private CategoryRepository repositoryCategories;
+	
 	public List<ProductDTO> findAll(){
 		List<Product> list = repository.findAll();
 		return list.stream().map(e -> new ProductDTO(e)).collect(Collectors.toList());
@@ -25,6 +33,24 @@ public class ProductService {
 	public ProductDTO findById(Long id){
 		Product entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
 		return new ProductDTO(entity);
+	}
+	
+	@Transactional
+	public ProductDTO insert(ProductCategoriesDTO dto) {
+		Product entity = dto.toEntity();
+		setProductCategories(entity, dto.getCategories());
+		entity = repository.save(entity);
+		return new ProductDTO(entity);
+	}
+
+	
+	private void setProductCategories(Product entity, List<CategoryDTO> categories) {
+		entity.getCategories().clear();
+		
+		for(CategoryDTO dto: categories) {
+			Category category = repositoryCategories.getOne(dto.getId());
+			entity.getCategories().add(category);
+		}
 	}
 	
 }
