e41842f4c2eb7ed8ac24f891a6798e2f83ea8f28
douglas s dos anjos <douglasdsda@gmail.com>
2019-10-01 22:00:41 -0300

    Custom user update validation: unique email

--- src/main/java/com/educaweb/course/dto/UserDTO.java
+++ src/main/java/com/educaweb/course/dto/UserDTO.java
@@ -2,14 +2,30 @@ package com.educaweb.course.dto;
 
 import java.io.Serializable;
 
-import com.educaweb.course.entities.User;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 
+import org.hibernate.validator.constraints.Length;
+
+import com.educaweb.course.entities.User;
+import com.educaweb.course.services.validations.UserUpdateValid;
+
+@UserUpdateValid
 public class UserDTO implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
 	private Long id;
+	
+	@NotEmpty(message = "cat't be empty")
+	@Length(min = 5, max = 80, message = "lenght must be betwen 5 and 80.")
 	private String name;
+
+	@NotEmpty(message = "cat't be empty")
+	@Email(message = "invalid email")
 	private String email;
+
+	@NotEmpty(message = "cat't be empty")
+	@Length(min = 8, max = 80, message = "lenght must be betwen 5 and 20.")
 	private String phone;
 
 	public UserDTO() {
@@ -23,14 +39,13 @@ public class UserDTO implements Serializable {
 		this.email = email;
 		this.phone = phone;
 	}
-	
+
 	public UserDTO(User entity) {
 		this.id = entity.getId();
 		this.name = entity.getName();
 		this.email = entity.getEmail();
 		this.phone = entity.getPhone();
 	}
-	
 
 	public Long getId() {
 		return id;
@@ -63,7 +78,7 @@ public class UserDTO implements Serializable {
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-	
+
 	public User toEntity() {
 		return new User(id, name, email, phone, null);
 	}
