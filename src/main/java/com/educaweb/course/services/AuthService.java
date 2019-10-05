b6b266d56c7fa929e2690e7d3fb0a12f5c19869b
douglas s dos anjos <douglasdsda@gmail.com>
2019-10-05 06:18:51 -0300

    Authentication

---
+++ src/main/java/com/educaweb/course/services/AuthService.java
@@ -0,0 +1,42 @@
+package com.educaweb.course.services;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.educaweb.course.dto.CredentialsDTO;
+import com.educaweb.course.dto.TokenDTO;
+import com.educaweb.course.security.JWTUtil;
+import com.educaweb.course.services.exceptions.JWTAuthenticationException;
+
+@Service
+public class AuthService {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	@Transactional(readOnly = true)
+	public TokenDTO authenticate(CredentialsDTO dto) {
+
+		try {
+			var authToken = new UsernamePasswordAuthenticationToken(dto.getEmail(), dto.getPassword());
+			authenticationManager.authenticate(authToken);
+
+			String token = jwtUtil.generateToken(dto.getEmail());
+
+			return new TokenDTO(dto.getEmail(), token);
+			
+		} catch (AuthenticationException e) {
+			 throw new JWTAuthenticationException("Bad credentials");
+		}
+
+	}
+
+}
--- src/main/java/com/educaweb/course/services/UserService.java
+++ src/main/java/com/educaweb/course/services/UserService.java
@@ -8,6 +8,9 @@ import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,7 +23,7 @@ import com.educaweb.course.services.exceptions.DatabaseException;
 import com.educaweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	@Autowired
 	private BCryptPasswordEncoder passwordEncode;
@@ -74,5 +77,17 @@ public class UserService {
 		entity.setName(dto.getName());
 		entity.setEmail(dto.getEmail());
 		entity.setPhone(dto.getPhone());
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		User user = repository.findByEmail(username);
+		
+		if (user == null) {
+			throw new UsernameNotFoundException(username);
+		}
+		
+		return user;
 	}
 }
