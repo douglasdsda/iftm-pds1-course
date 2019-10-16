dcb5e2230c6b790cee2ee5fa985572ad244e5e6b
Douglas S Anjos <douglasdsda@gmail.com>
2019-10-16 11:01:29 -0300

    Place order endpoint

--- src/main/java/com/educaweb/course/services/OrderService.java
+++ src/main/java/com/educaweb/course/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.educaweb.course.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -12,8 +13,12 @@ import com.educaweb.course.dto.OrderDTO;
 import com.educaweb.course.dto.OrderItemDTO;
 import com.educaweb.course.entities.Order;
 import com.educaweb.course.entities.OrderItem;
+import com.educaweb.course.entities.Product;
 import com.educaweb.course.entities.User;
+import com.educaweb.course.entities.enums.OrdersStatus;
+import com.educaweb.course.repositories.OrderItemRepository;
 import com.educaweb.course.repositories.OrderRepository;
+import com.educaweb.course.repositories.ProductRepository;
 import com.educaweb.course.repositories.UserRepository;
 import com.educaweb.course.services.exceptions.ResourceNotFoundException;
 
@@ -22,13 +27,19 @@ public class OrderService {
 
 	@Autowired
 	private OrderRepository repository;
-	
+
 	@Autowired
 	private AuthService authService;
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
+	@Autowired
+	private ProductRepository productRepository;
+
+	@Autowired
+	private OrderItemRepository orderItemRepository;
+
 	public List<OrderDTO> findAll() {
 		List<Order> list = repository.findAll();
 		return list.stream().map(e -> new OrderDTO(e)).collect(Collectors.toList());
@@ -39,8 +50,8 @@ public class OrderService {
 		authService.validateOwnOrderOrAdmin(entity);
 		return new OrderDTO(entity);
 	}
-	
-	public List<OrderDTO> findByClient(){
+
+	public List<OrderDTO> findByClient() {
 		User client = authService.authenticated();
 		List<Order> list = repository.findByClient(client);
 		return list.stream().map(e -> new OrderDTO(e)).collect(Collectors.toList());
@@ -51,7 +62,7 @@ public class OrderService {
 		Order order = repository.getOne(id);
 		authService.validateOwnOrderOrAdmin(order);
 		Set<OrderItem> set = order.getItems();
-		
+
 		return set.stream().map(e -> new OrderItemDTO(e)).collect(Collectors.toList());
 	}
 
@@ -60,5 +71,22 @@ public class OrderService {
 		User client = userRepository.getOne(clientId);
 		List<Order> list = repository.findByClient(client);
 		return list.stream().map(e -> new OrderDTO(e)).collect(Collectors.toList());
+	}
+
+	@Transactional
+	public OrderDTO placeOrder(List<OrderItemDTO> dto) {
+		User client = authService.authenticated();
+		Order order = new Order(null, Instant.now(), OrdersStatus.WAITING_PAYMENT, client);
+
+		for (OrderItemDTO itemDTO : dto) {
+			Product product = productRepository.getOne(itemDTO.getProductId());
+			OrderItem item = new OrderItem(order, product, itemDTO.getQuantity(), itemDTO.getPrice());
+			order.getItems().add(item);
+		}
+
+		repository.save(order);
+		orderItemRepository.saveAll(order.getItems());
+
+		return new OrderDTO(order);
 	}
 }
