dae8e05d241352335f241895be5924cb8cee3cd5
Douglas S Anjos <douglasdsda@gmail.com>
2019-10-18 06:41:09 -0300

    Update para java 8

--- src/main/java/com/educaweb/course/repositories/ProductRepository.java
+++ src/main/java/com/educaweb/course/repositories/ProductRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.educaweb.course.entities.Category;
@@ -16,14 +17,14 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 	@Transactional(readOnly = true)
 	@Query("SELECT DISTINCT obj FROM Product obj INNER join obj.categories cats where LOWER(obj.name) LIKE LOWER(CONCAT('%',:name, '%')) AND "
 			+ " cats IN :categories")
-	Page<Product> findByNameContainingIgnoreCaseAndCategoriesIn(String name, List<Category> categories, Pageable pageable);
+	Page<Product> findByNameContainingIgnoreCaseAndCategoriesIn(@Param("name") String name,@Param("categories") List<Category> categories, Pageable pageable);
 
 	@Transactional(readOnly = true)
 	@Query("SELECT obj FROM Product obj where LOWER(obj.name) LIKE LOWER(CONCAT('%',:name, '%'))")
-	Page<Product> findByNameContainingIgnoreCase(String name, Pageable pageable);
+	Page<Product> findByNameContainingIgnoreCase(@Param("name") String name, Pageable pageable);
 
 	@Transactional(readOnly = true)
 	@Query("SELECT obj FROM Product obj" + " INNER join obj.categories cats" + "	where :category in cats")
-	Page<Product> findByCategory(Category category, Pageable pageable);
+	Page<Product> findByCategory(@Param("category") Category category, Pageable pageable);
 
 }
